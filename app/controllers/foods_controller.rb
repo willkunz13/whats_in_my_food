f7557517781ec1358c19362df669eac92ddef897
jerry <jerry@jerrys-MBP.attlocal.net>
2020-03-30 12:03:57 -0400

    Add almost through api retrieval

--- app/controllers/foods_controller.rb
+++ app/controllers/foods_controller.rb
@@ -1,5 +1,11 @@
 class FoodsController < ApplicationController
 	def index
-		query = params[:q]
+		query = params[:q].gsub(' ', '%20')
+
+		conn = Faraday.new(url: "https://api.nal.usda.gov")
+		response = conn.get("/fdc/v1/search?api_key=#{ENV["MM_API_KEY"]}\&ingredients=#{query}&pageNumber=1")
+		json = JSON.parse(response.body, symbolize_names:true)
+		@foods = json[:foods]
+		binding.pry
 	end
 end
